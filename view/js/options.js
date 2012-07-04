1089948ebc080b5bfe3d74d8bf046179705f51cc
cattail2012@gmail.com <cattail2012@gmail.com@535783ea-835f-0aa2-06dc-b2db16d550a0>
2012-07-04 11:55:57 +0000

    git-svn-id: http://bookseeker.googlecode.com/svn/trunk@8 535783ea-835f-0aa2-06dc-b2db16d550a0

--- view/js/options.js
+++ view/js/options.js
@@ -1,13 +1,24 @@
 window.onload = function(){
+	var sites = document.querySelectorAll('input[name=defaultSite]');
+	var max = document.querySelector('input[name=maxNum]');
+	//init input
+	var storage = chrome.storage.sync;
+	storage.get(null,function(items){
+		var tmp = document.querySelector('input[value='+items.defaultSite+']');
+		tmp !== undefined ? (tmp.checked = true) : (sites[0].checked=true);
+
+		max.value = items.maxNum || 5;
+	});
+	
 	document.querySelector('#save').onclick = function(){
-		Array.prototype.forEach.call(document.querySelectorAll('input[name=defaultSite]'),function(input){
+		var defaultSite , maxNum;
+		Array.prototype.forEach.call(sites,function(input){
 			input.checked ? defaultSite = input.value : 0;
 		})
-		var maxNum = document.querySelector('input[name=maxNum]').value;
-		var storage = chrome.storage.sync;
+		maxNum = max.value;
 		storage.set({defaultSite:defaultSite,maxNum:maxNum},function(){
-			alert('saved');
-			console.log(storage.get(null,function(items){console.log(items);}));
+			alert('成功保存');
+			storage.get(null,function(items){console.log(items);});
 		});
 	};
 };
